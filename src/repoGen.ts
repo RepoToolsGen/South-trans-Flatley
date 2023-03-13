2a0c9a75bc1d26928c245c5a210eb8d996aafcd5
David Glodich <dglodich@veracode.com>
2023-03-13 15:28:18 -0400

    REPO-82: Minor cleanup and improved async handling

--- src/repoGen.ts
+++ src/repoGen.ts
@@ -9,7 +9,7 @@ import all from "./repoConfig.json";
 
 require("shelljs/global");
 
-const clonedReposDir: string = "clonedRepos";
+const localReposDir: string = "localRepos";
 
 /**
  * @description Checks if git is installed, silences shell output and creates
@@ -49,86 +49,87 @@ function getGitHubToken(): string {
  * If configured name field is empty, then generate a fake repo name.
  * @returns { void }
  */
-function processRepoConfig(): void {
+async function processRepoConfig(): Promise<void> {
   let repoCreationCounter: number = 0;
+  const inProgress: Promise<void>[] = [];
 
   repoList.forEach((repo) => {
     if (repo.count !== 0) {
-      const origRepoName = repo.name;
-      const repoNameDir: string = getClonedRepoName(repo.url);
+      const sourceRepoName: string = getSourceRepoName(repo.url);
 
       for (let i = 1; i <= repo.count; i++) {
-        repo.name = determineNewRepoName(origRepoName, i, repo.count);
-        const repoNew: RepoInfo = Object.assign({}, repo);
-        createRepo(repoNew, repoNameDir);
+        const targetRepoName = determineNewRepoName(repo.name, i, repo.count);
+        const repoNew: RepoInfo = { ...repo, name: targetRepoName };
+        inProgress.push(createTargetRepo(repoNew, sourceRepoName));
         repoCreationCounter++;
       }
     }
   });
-
+  await Promise.all(inProgress);
+  deleteLocalRepos();
   shell.echo(`TOTAL REPOSITORIES CREATED: ${repoCreationCounter}`);
 }
 
 /**
- * @description Clones the repo and creates new repo in GitHub
- * @param { repoInfo } repo repository info to be created
- * @param { repoNameDir } repoNameDir repository directory name
+ * @description Creates a new target repo, clones the source repo and pushes source repo into new target repo.
+ * @param { RepoInfo } repo repository info to be created
+ * @param { string } sourceRepoName source repository name
  * @returns { void }
  */
-function createRepo(repo: RepoInfo, repoNameDir: string): void {
+async function createTargetRepo(
+  repo: RepoInfo,
+  sourceRepoName: string
+): Promise<void> {
   shell.echo(
     `Processing ${repo.url} count: ${repo.count} ---> ${repo.organization}/${repo.name}`
   );
 
-  const sendPostRequest = async () => {
-    const body = {
-      name: repo.name,
-      description: repo.description,
-      private: repo.isPrivate,
-    };
-    try {
-      const response = await axios.post(
-        `https://api.github.com/orgs/${repo.organization}/repos`,
-        body,
-        {
-          headers: {
-            Authorization: "token " + gitHubToken,
-            Accept: "application/vnd.github+json",
-          },
-        }
-      );
-
-      runGitShellCommands(repo, repoNameDir);
-    } catch (err) {
-      const errors = err as Error | AxiosError;
-      if (axios.isAxiosError(errors)) {
-        shell.echo(
-          `ERROR ${repo.organization}/${repo.name} ${errors.response?.data.message} ${errors.response?.data.errors[0].message}`
-        );
-      }
-    }
+  const body = {
+    name: repo.name,
+    description: repo.description,
+    private: repo.isPrivate,
   };
-  sendPostRequest();
+  try {
+    await axios.post(
+      `https://api.github.com/orgs/${repo.organization}/repos`,
+      body,
+      {
+        headers: {
+          Authorization: "token " + gitHubToken,
+          Accept: "application/vnd.github+json",
+        },
+      }
+    );
+
+    copySourceRepoToTargetRepo(repo, sourceRepoName);
+  } catch (err) {
+    const errors = err as Error | AxiosError;
+    if (axios.isAxiosError(errors)) {
+      shell.echo(
+        `ERROR ${repo.organization}/${repo.name} ${errors.response?.data.message} ${errors.response?.data.errors[0].message}`
+      );
+    }
+  }
 }
 
 /**
- * @description Determine the name of the cloned repo from its URL
+ * @description Determine the name of the source repo from its URL
  * So if URL is https://gitlab.laputa.veracode.io/sca/srcclr/example-java-maven,
  * then return "example-java-maven"
  * @param { repoUrl } repoUrl repository URL
- * @returns { string } cloned repo name
+ * @returns { string } source repo name
  */
-function getClonedRepoName(repoUrl: string): string {
+function getSourceRepoName(repoUrl: string): string {
   const url = new URL(repoUrl);
-  const clonedRepoName = url.pathname.substring(
+  const sourceRepoName = url.pathname.substring(
     url.pathname.lastIndexOf("/") + 1,
     url.pathname.length
   );
-  return clonedRepoName;
+  return sourceRepoName;
 }
 
 /**
- * @description Generate a fake respository name using npm faker module
+ * @description Generate a fake repository name using npm faker module
  * using format of "random word + random word + random last name"
  * @returns { string } fake repo name
  */
@@ -167,18 +168,21 @@ function determineNewRepoName(
 }
 
 /**
- * @description Runs the shellJS git commands to create a new repo.
+ * @description Runs the shellJS git commands to copy source repo to new target repo.
  * @param { RepoInfo } repo repository info to be created
- * @param { string } repoNameDir repo name directory
+ * @param { string } sourceRepoName source repo name
  * @returns { void }
  */
-function runGitShellCommands(repo: RepoInfo, repoNameDir: string): void {
+function copySourceRepoToTargetRepo(
+  repo: RepoInfo,
+  sourceRepoName: string
+): void {
   try {
     // if repo not already created, then git clone
-    if (!fs.existsSync(path.resolve(clonedReposDir + "/" + repoNameDir))) {
+    if (!fs.existsSync(path.resolve(localReposDir + "/" + sourceRepoName))) {
       if (
         shell.exec(`git clone ${repo.url}`, {
-          cwd: path.resolve(clonedReposDir),
+          cwd: path.resolve(localReposDir),
         }).code !== 0
       ) {
         shell.echo(`git clone failure`);
@@ -188,7 +192,7 @@ function runGitShellCommands(repo: RepoInfo, repoNameDir: string): void {
 
     if (
       shell.exec(`git remote remove origin`, {
-        cwd: path.resolve(clonedReposDir + "/" + repoNameDir),
+        cwd: path.resolve(localReposDir + "/" + sourceRepoName),
       }).code !== 0
     ) {
       shell.echo(`git remote remove origin failed`);
@@ -198,7 +202,7 @@ function runGitShellCommands(repo: RepoInfo, repoNameDir: string): void {
     if (
       shell.exec(
         `git remote add origin https://github.com/${repo.organization}/${repo.name}.git`,
-        { cwd: path.resolve("clonedRepos/" + repoNameDir) }
+        { cwd: path.resolve("clonedRepos/" + sourceRepoName) }
       ).code !== 0
     ) {
       shell.echo(`git remote add origin failed`);
@@ -207,7 +211,7 @@ function runGitShellCommands(repo: RepoInfo, repoNameDir: string): void {
 
     if (
       shell.exec(`git branch -M main`, {
-        cwd: path.resolve(clonedReposDir + "/" + repoNameDir),
+        cwd: path.resolve(localReposDir + "/" + sourceRepoName),
       }).code !== 0
     ) {
       shell.echo(`git branch failed`);
@@ -216,7 +220,7 @@ function runGitShellCommands(repo: RepoInfo, repoNameDir: string): void {
 
     if (
       shell.exec(`git push -u origin main`, {
-        cwd: path.resolve(clonedReposDir + "/" + repoNameDir),
+        cwd: path.resolve(localReposDir + "/" + sourceRepoName),
       }).code !== 0
     ) {
       shell.echo(`git push failed`);
@@ -239,3 +243,10 @@ const gitHubToken = getGitHubToken();
 
 // Process and create repositories according to repoConfig.json file
 processRepoConfig();
+
+/**
+ * @description TODO:
+ */
+function deleteLocalRepos() {
+  // TODO: implement delete.
+}
